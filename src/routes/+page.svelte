406bb78ff77587b06e7d989b1e1aceccf7aabde6
dindles <114931816+dindles@users.noreply.github.com>
2025-12-29 15:29:37 +0200

    dindles.net

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,23 @@
 </script>
 
 <h1>dindles</h1>
-<p>a chill website</p>
+<p>p.l.u.r.</p>
 
 <Neko type="kina" />
 
 <style>
+  global:html {
+    margin: 0;
+    padding: 0;
+    font-family: 'Courier New', Courier, monospace;
+    background-color: #f0f0f0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+
   h1 {
     color: #3e3e3e;
     text-transform: uppercase;
